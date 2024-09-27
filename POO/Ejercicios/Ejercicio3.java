10b12f309f7531db5ffc0d3c57ed2c4a9f780f61
Diego <taysonm895@gmail.com>
2024-09-27 19:57:42 -0300

    mas

--- POO/Ejercicios/Ejercicio3.java
+++ POO/Ejercicios/Ejercicio3.java
@@ -2,53 +2,22 @@ package POO.Ejercicios;
 
 import java.util.Scanner;
 
-public class Ejercicio3 {
+public class Ejercicio3_V2 {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
         
-        System.out.println("Ingrese la fraccion");
-        System.out.print("Ingrese el numerador: ");
-        int numf1 = sc.nextInt();
-        System.out.print("Ingrese el denominador: ");
-        int denf1 = sc.nextInt();
+        // Fracciones creadas
+        Fraccion fraccion1 = new Fraccion(10, 5);
+        Fraccion fraccion2 = new Fraccion(8, 2);
         
-        Fraccion fraccion1 = new Fraccion(numf1, denf1);
+        // Operaciones
+        // Suma de Fracciones
+        Fraccion resultadoSuma = fraccion1.sumar(fraccion2);
+        System.out.println("Resultado de la suma: " + resultadoSuma);
         
-        System.out.print("Quieres cargar otra fraccion? (SI / NO): ");
-        String decision = sc.next();
-        
-        if(decision.equalsIgnoreCase("si")){
-            System.out.println("Ingrese la otra fraccion");
-            System.out.print("Ingrese el numerador: ");
-            int numf2 = sc.nextInt();
-            System.out.print("Ingrese el denominador: ");
-            int denf2 = sc.nextInt();
-            
-            System.out.print("Seleccione la operacion a realizar (+ *): ");
-            String operacion = sc.next();
-            
-            String resultado;
-            
-            switch(operacion){
-                case "+":
-                    resultado = fraccion1.sumar(numf2, denf2);
-                    System.out.println("Suma de: " + fraccion1 + " + " + numf2 + "/" + denf2 + " = " + resultado);
-                    break;
-                case "*":
-                    resultado = fraccion1.multiplicar(numf2, denf2);
-                    System.out.println("Producto de: " + fraccion1 + " * " + numf2 + "/" + denf2 + " = " + resultado);
-                    break;
-                default:
-                    System.out.println("Opcion incorrecta");
-            }
-            
-        }else{
-            System.out.println("Fraccion ingresada: " + fraccion1);
-        }
-        
-        
+        // Multiplicacion de Fracciones
+        Fraccion resultadoMultiplicacion = fraccion1.multiplicar(fraccion2);
+        System.out.println("Resultado de la multiplicacion: " + resultadoMultiplicacion);
         
     }
     
@@ -56,14 +25,98 @@ public class Ejercicio3 {
 
 class Fraccion {
     
+    // Atributos
     private int numerador;
     private int denominador;
+    
+    // Constructores
+    public Fraccion(){
+        
+    }
 
     public Fraccion(int numerador, int denominador) {
         this.numerador = numerador;
         this.denominador = denominador;
     }
 
+    public Fraccion sumar(Fraccion otra_fraccion){
+        
+        // Intancio un objeto fraccion con su constructor sin parametro
+        Fraccion fraccionResultado = new Fraccion();
+        
+        // Creo variables y realizo las operaciones
+        int numeradorResultado, denominadorResultado;
+        
+        // Operaciones basada en suma de fracciones, dos posibilidades
+        if(this.numerador == otra_fraccion.numerador){
+            numeradorResultado = this.numerador + otra_fraccion.numerador;
+            denominadorResultado = this.numerador;
+        }else{
+            numeradorResultado = (this.numerador * otra_fraccion.denominador) + (otra_fraccion.numerador * this.denominador);
+            denominadorResultado = this.denominador * otra_fraccion.denominador;
+        }
+        
+        // Inicializo los atributos del objeto fraccionResultado con los metodos SETTER
+        fraccionResultado.setNumerador(numeradorResultado);
+        fraccionResultado.setDenominador(denominadorResultado);
+        
+        // Retorno la fraccion resultado
+        return fraccionResultado;
+        
+    }
+    
+    public Fraccion multiplicar(Fraccion otra_fraccion){
+        
+        // Intancio un objeto fraccion con su constructor sin parametro
+        Fraccion fraccionResultado = new Fraccion();
+        
+        // Creo variables y realizo las operaciones
+        int numeradorResultado = this.numerador * otra_fraccion.numerador;
+        int denominadorResultado = this.denominador * otra_fraccion.denominador;
+        
+        // Inicializo los atributos del objeto fraccionResultado con los metodos SETTER
+        fraccionResultado.setNumerador(numeradorResultado);
+        fraccionResultado.setDenominador(denominadorResultado);
+        
+        // Retorno la fraccion resultado
+        return fraccionResultado;
+        
+    }
+
+    // Otra forma 
+    //-----------------------------------------------------------------------------------------------------------------
+    /*public Fraccion sumar(Fraccion otra_fraccion){
+        
+        // Creo variables y realizo las operaciones
+        int numeradorResultado, denominadorResultado;
+        
+        // Operaciones basada en suma de fracciones, dos posibilidades
+        if(this.numerador == otra_fraccion.numerador){
+            numeradorResultado = this.numerador + otra_fraccion.numerador;
+            denominadorResultado = this.numerador;
+        }else{
+            numeradorResultado = (this.numerador * otra_fraccion.denominador) + (otra_fraccion.numerador * this.denominador);
+            denominadorResultado = this.denominador * otra_fraccion.denominador;
+        }
+        
+        // Retorno la fraccion resultado
+        return new Fraccion(numeradorResultado, denominadorResultado);
+        
+    }
+    
+    public Fraccion multiplicar(Fraccion otra_fraccion){
+        
+        // Creo variables y realizo las operaciones
+        int numeradorResultado = this.numerador * otra_fraccion.numerador;
+        int denominadorResultado = this.denominador * otra_fraccion.denominador;
+        
+        // Retorno la fraccion resultado
+        return new Fraccion(numeradorResultado, denominadorResultado);
+        
+    }*/
+    //-----------------------------------------------------------------------------------------------------------------
+    
+    // Metodos Getter y Setter
     public int getNumerador() {
         return numerador;
     }
@@ -79,35 +132,12 @@ class Fraccion {
     public void setDenominador(int denominador) {
         this.denominador = denominador;
     }
-
+    
+    // Metodo ToString para devolver el resultado
     @Override
     public String toString() {
         return numerador + "/" + denominador;
     }
     
-    public String sumar(int numerador, int denominador){
-        
-        int numeradorResultado, denominadorResultado;
-        
-        if(this.numerador == numerador){
-            numeradorResultado = this.numerador + numerador;
-            denominadorResultado = this.denominador;
-        }else{
-            numeradorResultado = (this.numerador*denominador) + (numerador*this.denominador);
-            denominadorResultado = this.denominador * denominador;
-        }
-        
-        return numeradorResultado + "/" + denominadorResultado;
-        
-    }
-    
-    public String multiplicar(int numerador, int denominador){
-        
-        int numeradorResultado = this.numerador * numerador;
-        int denominadorResultado = this.denominador * denominador;
-        
-        return numeradorResultado + "/" + denominadorResultado;
-        
-    }
-    
 }
+
