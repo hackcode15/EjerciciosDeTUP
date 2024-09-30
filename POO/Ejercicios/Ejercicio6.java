b166f54dbf123787c71efefda217dd7f735f25e2
Diego <taysonm895@gmail.com>
2024-09-29 22:46:47 -0300

    modifico el ejercicio 6 de POO

--- POO/Ejercicios/Ejercicio6.java
+++ POO/Ejercicios/Ejercicio6.java
@@ -1,23 +1,25 @@
 package POO.Ejercicios;
 
-public class Ejercicio6 {
-
-    public static void main(String[] args) {
-        
-        Corcho corcho = new Corcho("Bodega 1", true);
-        Botella botella = new Botella(corcho);
-        
-        SacaCorchos sacacorcho = new SacaCorchos();
-        
-        sacacorcho.destapar(botella);
-        //sacacorcho.destapar(botella);
-        
+class Botella {
+    
+    private Corcho corcho;
+    
+    public Botella(Corcho corcho){
+        this.corcho = corcho;
+    }
+    
+    public Corcho getCorcho(){
+        return corcho;
+    }
+    
+    public void setCorcho(Corcho corcho){
+        this.corcho = corcho;
     }
     
 }
 
 class Corcho {
-
+    
     private String bodega;
     private boolean enBotella;
     
@@ -34,7 +36,7 @@ class Corcho {
         this.bodega = bodega;
     }
 
-    public boolean getEnBotella() {
+    public boolean isEnBotella() {
         return enBotella;
     }
 
@@ -44,60 +46,91 @@ class Corcho {
     
 }
 
-class Botella {
-    
-    private Corcho corcho;
-    
-    public Botella(Corcho corcho){
-        this.corcho = corcho;
-    }
-
-    public Corcho getCorcho() {
-        return corcho;
-    }
-
-    public void setCorcho(Corcho corcho) {
-        this.corcho = corcho;
-    }
-    
-    
-    
-}
-
-class SacaCorchos {
+class Sacacorcho {
     
     private boolean tieneCorcho;
     
-    public SacaCorchos(){
-        this.tieneCorcho = false;
-    }
-    
-    public boolean getTieneCorcho(){
-        return tieneCorcho;
+    public Sacacorcho(){
+        tieneCorcho = false;
     }
     
     public void destapar(Botella botella){
-        if(botella.getCorcho().getEnBotella()){
-            System.out.println("Error: La botella no tiene corcho para sacar");
-            return;
-        }
         
         if(!tieneCorcho){
-            botella.getCorcho().setEnBotella(false);
-            System.out.println("Se ha destapado la botella");
-            tieneCorcho = true;
+            
+            if(botella.getCorcho().isEnBotella()){
+                botella.getCorcho().setEnBotella(false);
+                tieneCorcho = true;
+                System.out.println("Corcho sacado\n");
+            }else{
+                System.out.println("Error: la botella no tiene corcho\n");
+            }
+            
         }else{
-            System.out.println("Error: El SacaCorcho esta ocupado");
+            System.out.println("Error: el Sacacorchos no esta limpio\n");
         }
+        
     }
     
     public void limpiar(){
+        
         if(tieneCorcho){
             tieneCorcho = false;
-            System.out.println("SacaCorcho limpiado correctamente");
+            System.out.println("Sacacorcho limpiado\n");
         }else{
-            System.out.println("Error: El SacaCorcho no tiene ningun corcho para limpiar");
+            System.out.println("Error: el sacacorchos ya esta limpio\n");
         }
+        
+    }
+    
+    public boolean isTieneCorcho(){
+        return tieneCorcho;
+    }
+    
+    public void setTieneCorcho(boolean tieneCorcho){
+        this.tieneCorcho = tieneCorcho;
+    }
+    
+}
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        
+        // Instancio un Saca Corcho
+        Sacacorcho miSacacorcho = new Sacacorcho();
+        
+        // Instancio un corcho
+        Corcho miCorcho = new Corcho("Balbo", true); // Corcho puesto en botella
+        
+        // Instancio una botella
+        Botella miBotella = new Botella(miCorcho);
+        
+        // Verifico si la botella tiene corcho
+        System.out.println("Tiene corcho?: " + miBotella.getCorcho().isEnBotella() + "\n");
+        
+        // Sacamos el corcho
+        miSacacorcho.destapar(miBotella);
+        
+        // Verifico nuevamente si la botella tiene corcho
+        System.out.println("Tiene corcho?: " + miBotella.getCorcho().isEnBotella() + "\n");
+        
+        // Prueba de los distintos errores
+        
+        // Sacacorcho sin limpiar
+        // Intento sacar el corcho a una botella sin corcho
+        miSacacorcho.destapar(miBotella);
+        
+        // Limpio el Sacacorcho
+        miSacacorcho.limpiar();
+        
+        // Sacacorcho limpiado
+        // Intento sacar el corcho a una botella sin corcho
+        miSacacorcho.destapar(miBotella);
+        
+        // Intento limpiar un corcho que ya esta limpio
+        miSacacorcho.limpiar();
+        
     }
     
 }
